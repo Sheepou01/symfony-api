0f1e37d6de04b998b52e773d1929a4ff96dd618c
Guizmoooe <guillaumemarquesdev@gmail.com>
2019-01-23 16:55:04 +0100

    POST User inscription test 2

--- src/Controller/FOSUserController.php
+++ src/Controller/FOSUserController.php
@@ -9,20 +9,24 @@ use FOS\RestBundle\Controller\FOSRestController;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints\DateTime;
+use App\Repository\RoleRepository;
+
 
 class FOSUserController extends FOSRestController{
 
     /**
      * @Rest\Post("/api/signup")
-     * @Rest\View
+     * @Rest\View(StatusCode=201)
      * @ParamConverter("user", converter="fos_rest.request_body")
      */
 
-    public function signup(User $user){
-
+    public function signup(User $user, RoleRepository $role){
+        $roleUser = $role->findOneBy(['code' => 'ROLE_USER']);
         $em = $this->getDoctrine()->getManager();
+        $user->setCreatedAt(new \DateTime);
+        $user->setRole($roleUser);
         $em->persist($user);
         $em->flush();
-        return new Response($user);
+        return $user;
     }
 }

c16a9d6dced6d158123e90a7567923f04fa00e8b
Guizmoooe <guillaumemarquesdev@gmail.com>
2019-01-23 17:31:47 +0100

    inscription en bdd ok

--- src/Controller/FOSUserController.php
+++ src/Controller/FOSUserController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Constraints\DateTime;
 use App\Repository\RoleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class FOSUserController extends FOSRestController{
@@ -20,13 +23,33 @@ class FOSUserController extends FOSRestController{
      * @ParamConverter("user", converter="fos_rest.request_body")
      */
 
-    public function signup(User $user, RoleRepository $role){
+    public function signup(User $user, RoleRepository $role, UserPasswordEncoderInterface $encoder){
+
+       
+
         $roleUser = $role->findOneBy(['code' => 'ROLE_USER']);
         $em = $this->getDoctrine()->getManager();
         $user->setCreatedAt(new \DateTime);
+        // dd($user->getPassword());
+        $hash = $encoder->encodePassword($user,$user->getPassword());
+        $user->setPassword($hash);
         $user->setRole($roleUser);
         $em->persist($user);
         $em->flush();
-        return $user;
+        return new Response('Inscription effectuée');
     }
+
+    // /**
+    //  * @Rest\Post("/api/signin")
+    //  * @Rest\View(StatusCode=202)
+    //  * @Rest\Get("/api/signin")
+    //  */
+    // public function signin(AuthenticationUtils $authenticationUtils){
+    //      // get the login error if there is one
+    //      $error = $authenticationUtils->getLastAuthenticationError();
+    //      // last username entered by the user
+    //      $lastUsername = $authenticationUtils->getLastUsername();
+    //      dd($this->getUser());
+    //      return new Response('Connexion effectuée');
+    // }
 }

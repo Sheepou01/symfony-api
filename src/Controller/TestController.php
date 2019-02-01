58d0c29998f39455bc3bad3345b6144fcf2b25d5
Guizmoooe <guillaumemarques@live.fr>
2019-02-01 11:57:40 +0100

    test controller pour token

---
+++ src/Controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/api/test", name="test")
+     */
+    public function index()
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+}

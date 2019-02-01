50139477d0e760ed72e3e60be231ed0494b05b0b
Guizmoooe <guillaumemarques@live.fr>
2019-02-01 09:14:07 +0100

    requete quizz

--- src/Controller/FOSQuizController.php
+++ src/Controller/FOSQuizController.php
@@ -1,19 +1,25 @@
 <?php
-
 namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
-class FOSQuizController extends AbstractController
-{
+use App\Entity\Quizz;
+use App\Repository\QuizzRepository;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\FOSRestController;
+class FOSQuizzController extends FOSRestController{
     /**
-     * @Route("/quiz", name="quiz")
+     * @GET(
+     *  path="/api/quizz",
+     * name="show_quizz",
+     * )
+     * @View
      */
-    public function index()
-    {
-        return $this->render('quiz/index.html.twig', [
-            'controller_name' => 'QuizController',
-        ]);
+    public function showQuizz(QuizzRepository $quizzRepository){
+        
+        $quizz = $quizzRepository->find(1);
+      
+        
+        return $quizz;
+        
     }
 }

f1ea73b4e5b3f767c28c3805d8560711e0e894e6
Guizmoooe <guillaumemarques@live.fr>
2019-02-01 09:17:12 +0100

    modif requete

--- src/Controller/FOSQuizzController.php
+++ src/Controller/FOSQuizzController.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Controller;
+
 use App\Entity\Quizz;
 use App\Repository\QuizzRepository;
 use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
+
 class FOSQuizzController extends FOSRestController{
     /**
      * @GET(
